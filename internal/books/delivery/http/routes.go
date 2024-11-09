7584098d7d2823a42197d17c08d64c5a6289b332
Ekaterina <litra1122@gmail.com>
2024-11-09 23:27:36 +0300

    permissions middleware added

--- internal/books/delivery/http/routes.go
+++ internal/books/delivery/http/routes.go
@@ -9,9 +9,9 @@ import (
 )
 
 func RegisterBookRoutes(router *httprouter.Router, h books.Handlers, mw *middleware.MiddlewareManager) {
-	router.HandlerFunc(http.MethodGet, "/v1/books", h.List())
-	router.HandlerFunc(http.MethodPost, "/v1/books", h.Create())
-	router.HandlerFunc(http.MethodGet, "/v1/books/:id", h.Get())
-	router.HandlerFunc(http.MethodPatch, "/v1/books/:id", h.Update())
-	router.HandlerFunc(http.MethodDelete, "/v1/books/:id", h.Delete())
+	router.HandlerFunc(http.MethodGet, "/v1/books", mw.RequirePermission("books:read", h.List()))
+	router.HandlerFunc(http.MethodPost, "/v1/books", mw.RequirePermission("books:write", h.Create()))
+	router.HandlerFunc(http.MethodGet, "/v1/books/:id", mw.RequirePermission("books:read", h.Get()))
+	router.HandlerFunc(http.MethodPatch, "/v1/books/:id", mw.RequirePermission("books:write", h.Update()))
+	router.HandlerFunc(http.MethodDelete, "/v1/books/:id", mw.RequirePermission("books:write", h.Delete()))
 }
--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -19,18 +19,18 @@ import (
 	usersRepository "bookloop.net/internal/users/repository"
 
 	"bookloop.net/internal/middleware"
-	"bookloop.net/pkg/error_responses"
+	erp "bookloop.net/pkg/error_responses"
 )
 
 func (s *Server) RegisterHandlers() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		error_responses.NotFoundResponse(w, r, s.logger)
+		erp.NotFoundResponse(w, r, s.logger)
 	})
 
 	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		error_responses.MethodNotAllowedResponse(w, r, s.logger)
+		erp.MethodNotAllowedResponse(w, r, s.logger)
 	})
 
 	// Init repositories
